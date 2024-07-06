6e3b14cae0a437e9a8ee170112b10b0e397bca3a
Devin Garcia <devinmgarcia@gmail.com>
2024-07-06 19:08:47 -0400

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Photo from "@/components/Photo";
 import Social from "@/components/Social";
-import { Button } from "@/components/ui/button";
-import { FiDownload } from "react-icons/fi";
 const Home = () => {
   return (
     <section className="h-full">
