cf70ba0ead2fbd83e2fb5f050d9c2d1e9db1001b
Devin Garcia <devinmgarcia@gmail.com>
2024-07-06 19:18:28 -0400

    change page.tsx

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { useEffect, useState } from "react";
+"use client";
+import { useEffect, useState } from 'react';
 
 interface PhotoAttributes {
   title: string;
@@ -25,7 +25,7 @@ interface ApiResponse {
 const Photos: React.FC = () => {
   const [photos, setPhotos] = useState<Photo[]>([]);
   const token = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
-  const url = "https://dannisunday-api-72961c617c59.herokuapp.com"
+  const url = "https://dannisunday-api-72961c617c59.herokuapp.com";
 
   useEffect(() => {
     fetch(`${url}/api/photos?populate=image`, {
@@ -35,20 +35,21 @@ const Photos: React.FC = () => {
     })
     .then((response) => response.json())
     .then((data: ApiResponse) => setPhotos(data.data))
-    .catch((error) => console.error("Error fetching data:", error));
+    .catch((error) => console.error('Error fetching data:', error));
   }, [token]);
-
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-    {photos.map((photo) => (
-      <div key={photo.id} className="overflow-hidden shadow-lg rounded-lg">
-        <img className="w-full h-auto" src={`${url}${photo.attributes.image.data.attributes.url}`} alt={photo.attributes.title} />
-        <div className="px-6 py-4">
+      {photos.map((photo) => (
+        <div key={photo.id} className="overflow-hidden shadow-lg rounded-lg">
+          <img className="w-full h-auto" src={`${url}${photo.attributes.image.data.attributes.url}`} alt={photo.attributes.title} />
+          <div className="px-6 py-4">
+            <h3 className="text-gray-900 font-bold text-xl mb-2">{photo.attributes.title}</h3>
+            <p className="text-gray-700 text-base">{photo.attributes.description}</p>
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
+      ))}
+    </div>
   );
 };
 
