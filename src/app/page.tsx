b9b28c3ef72a94960fcf51a16138c68c2b0026af
Devin Garcia <devinmgarcia@gmail.com>
2024-07-06 18:59:47 -0400

    added hosted strapi cms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,6 @@ const Home = () => {
               I take photos of cute animals.
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
-              >
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
               <div className="mb-8 xl:mb-0">
                 <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,7 +1,55 @@
-const Work = () => {
-  return (
-    <div>work page</div>
-  )
+"use client"
+import { useEffect, useState } from 'react';
+
+interface PhotoAttributes {
+  title: string;
+  description: string;
+  image: {
+    data: {
+      attributes: {
+        url: string;
+      };
+    };
+  };
 }
 
-export default Work
+interface Photo {
+  id: number;
+  attributes: PhotoAttributes;
+}
+
+interface ApiResponse {
+  data: Photo[];
+}
+
+const Photos: React.FC = () => {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const token = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
+  const url = "https://dannisunday-api-72961c617c59.herokuapp.com"
+
+  useEffect(() => {
+    fetch(`${url}/api/photos?populate=image`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then((response) => response.json())
+    .then((data: ApiResponse) => setPhotos(data.data))
+    .catch((error) => console.error('Error fetching data:', error));
+  }, [token]);
+
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    {photos.map((photo) => (
+      <div key={photo.id} className="overflow-hidden shadow-lg rounded-lg">
+        <img className="w-full h-auto" src={`${url}${photo.attributes.image.data.attributes.url}`} alt={photo.attributes.title} />
+        <div className="px-6 py-4">
+        </div>
+      </div>
+    ))}
+  </div>
+  );
+};
+
+export default Photos;
